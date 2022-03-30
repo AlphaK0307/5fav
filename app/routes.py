39e7928519502d6ebf4882efa758c937dff5f376
AlphaK0307 <keyurpatel1121@gmail.com>
2022-03-29 21:05:52 -0500

    commit

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,33 @@ from flask import render_template
 def index():
     title='Home'
     user= {'id': 1, 'username':'Patel', 'email':'keyurpatel1121@gmail.com'}
-    return render_template('index.html', current_user=user)
+    posts = [
+        {
+            'id': 1,
+            'title': 'March Madness',
+            'body': 'The NCAA tournament never seems to disappoint!',
+            'author': 'cbbfan'
+        },
+        {
+            'id': 2,
+            'title': 'Oscars',
+            'body': 'I think that Coda might win Best Picture this year. I cried the whole time.',
+            'author': 'BuffMovieBuff123'
+        },
+        {
+            'id': 3,
+            'title': 'Python',
+            'body': 'Python is by far my favorite programming language. It is so neat!',
+            'author': 'code_is_life'
+        },
+        {
+            'id': 4,
+            'title': 'Flask',
+            'body': 'My favorite microframework! And this is only the beginning.',
+            'author': 'CodingTemple4L'
+        }
+    ]
+    return render_template('index.html', current_user=user, title=title, posts=posts)
 
 
 @app.route('/test')
